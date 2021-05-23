df8efb77d5e9c8714a71e85e3793feb8fda81121
hoai.truong <hoaitruong@hoaitruong.local>
2021-05-23 23:57:30 +0800

    ! Add cors

--- routes/section.js
+++ routes/section.js
@@ -4,10 +4,19 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 const SectionController = require("../controllers/SectionController");
+const cors = require("cors");
 
-router.post("/add-section", SectionController.addDataSection);
-router.get("/get-section/:sectionName", SectionController.getDataSection);
-router.post("/edit-section/:sectionName", SectionController.addDataSection);
-router.delete("/remove/:sectionName", SectionController.removeSection);
-router.post("/send-email", SectionController.sendEmail);
+router.post("/add-section", cors(), SectionController.addDataSection);
+router.get(
+  "/get-section/:sectionName",
+  cors(),
+  SectionController.getDataSection
+);
+router.post(
+  "/edit-section/:sectionName",
+  cors(),
+  SectionController.addDataSection
+);
+router.delete("/remove/:sectionName", cors(), SectionController.removeSection);
+router.post("/send-email", cors(), SectionController.sendEmail);
 module.exports = router;
