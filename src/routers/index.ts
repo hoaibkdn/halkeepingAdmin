d292063670cefd1e72899ef35b2a3b7567366ca8
hoaitruong <hoaibkdn2012@gmail.com>
2024-02-16 19:52:43 -0800

    chore: create province api

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { login } from '../controllers/auth.controllers';
 import { getListUser } from '../controllers/user.controlers';
+import { getProvinces } from './../controllers/province.controllers';
 
 const router = Router();
 
 // Auth
 router.post('/login', login);
 router.get('/users', getListUser);
+
+router.get('/provinces', getProvinces);
 
 export default router;
