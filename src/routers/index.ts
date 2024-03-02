cabf77e2de596b82d70a47c6187d57a2cc9ea75c
Kim Hue <kimhuebk94@gmail.com>
2024-03-02 14:22:48 +0700

    Fix api doc

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -6,32 +6,37 @@ import { getProvinces } from './../controllers/province.controllers';
 
 const router = Router();
 
-// Auth
 router.post('/login', login);
 router.get('/users', getListUser);
 
 router.get('/provinces', getProvinces);
 
-// job
 /**
- * @api {post} /job create new job
+ * @api {post} /job/create create new job
  * @apiName createNewJob
  * @apiGroup Job
- * @param {
- *  @name: "A",
- *  @phone: "096578988"
- *  @email: "lan@gmail.com",
- *  @address: "Nguyen thi minh khai",
- *  @workdate: "22-04-2024, 16:23",
- *  @numberOfClean: 2,
- *  @durationEachClean: 80, // minute
- *  @method: "cash",
- *  @tools: [{
- *    @toolId: "1",
- *    @realPrice": 34000
- *  }]
- * }
- * @apiSuccess {Job} new job
+ * @apiParam {String} name Customer's name: "Nguyen van A".
+ * @apiParam {String} phone Customer's unique phone: "096578988".
+ * @apiParam {String} email Customer's unique email: "lan@gmail.com".
+ * @apiParam {String} address Customer's Addess:  "Nguyen thi minh khai Quan 1, Thanh pho HCM".
+ * @apiParam {String} workdate date and time that customer request cleaner to come: "22-04-2024, 16:23".
+ * @apiParam {Number} numberOfclean amount of clean needed: 2.
+ * @apiParam {Number} durationEachClean amount of time of each clean and minute unit: 80.
+ * @apiParam {String} method Customer will pay: "cash" (cash or credit).
+ * @apiParam {Object[]} tools Array of tool objects.
+ * @apiParam {String} tools.toolId id of the user.
+ * @apiParam {String} tools.realPrice real price of the tool.
+ *
+ * @apiSuccess {Object} data
+ * @apiSuccess {Object} data.job new job
+ * @apiSuccess {String} data.job.customerId customer's id.
+ * @apiSuccess {String} data.job.workDate date and time of job.
+ * @apiSuccess {String} data.job.payment method of job.
+ * @apiSuccess {Boolean} data.job.isPay is pay of job.
+ * @apiSuccess {Number} data.job.pricePerHour price of per house
+ * @apiSuccess {Number} data.job.duration total time of job (unit is minute)
+ * @apiSuccess {Number} data.job.total total money of job
+ * @apiSuccess {Number} data.job.id Job's id
  */
 router.post('/job/create', createNewJob);
 
